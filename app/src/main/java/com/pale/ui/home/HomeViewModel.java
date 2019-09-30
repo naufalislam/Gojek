cb5b0ad39d5de13b60fa3d7f381fc1a2017e236d
Naufal Islam <naufalislam.16@gmail.com>
2019-10-01 00:10:04 +0700

    Initial commit

---
+++ app/src/main/java/com/pale/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.pale.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Ini Adalah Halaman Home");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
